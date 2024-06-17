e7e69d057077caefab9e36571411a853872ba610
sceddd <socdia07@gmail.com>
2024-06-17 17:51:32 +0700

    add threshold branch

---
+++ LOST/object_discovery.py
@@ -0,0 +1,185 @@
+# Copyright 2021 - Valeo Comfort and Driving Assistance - Oriane Siméoni @ valeo.ai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import scipy.ndimage
+from sklearn.cluster import DBSCAN
+import numpy as np
+import matplotlib.pyplot as plt
+from datasets import bbox_iou
+
+
+def lost(feats, dims, scales, init_image_size, k_patches=100, dynamic_thres=False, dbscan=True, add_thres=0):
+    """
+    Implementation of LOST method.
+    Inputs
+        feats: the pixel/patch features of an image
+        dims: dimension of the map from which the features are used
+        scales: from image to map scale
+        init_image_size: size of the image
+        k_patches: number of k patches retrieved that are compared to the seed at seed expansion
+    Outputs
+        pred: box predictions
+        A: binary affinity matrix
+        scores: lowest degree scores for all patches
+        seed: selected patch corresponding to an object
+    """
+    A = (feats @ feats.transpose(1, 2)).squeeze()
+
+    sorted_patches, scores, jumps = patch_scoring(A, dynamic_thres, k_patches=k_patches, add_thres=add_thres)
+
+    if k_patches == -1:
+        not_potentials_xy = [np.unravel_index(p.cpu(), dims) for p in sorted_patches]
+        not_potentials_xy_filtered = dbscan_filter(not_potentials_xy) if dbscan else not_potentials_xy
+        not_potentials_filtered_index = [np.ravel_multi_index(p, dims) for p in not_potentials_xy_filtered]
+    else:
+        potentials = sorted_patches[:k_patches]
+        not_potentials_xy = [np.unravel_index(p.cpu(), dims) for p in potentials]
+        not_potentials_xy_filtered = dbscan_filter(not_potentials_xy) if dbscan else not_potentials_xy
+        not_potentials_filtered_index = [np.ravel_multi_index(p, dims) for p in not_potentials_xy_filtered]
+
+    pred, _ = detect_box(dims, scales=scales, object_patches=not_potentials_filtered_index,
+                         initial_im_size=init_image_size[1:])
+
+    return np.asarray(pred), A, scores, not_potentials_filtered_index, jumps, len(sorted_patches)
+
+
+def loop_thres(feats, dims, scales, init_image_size, k_patches=100, dynamic_thres=False, dbscan=True, add_thres=0):
+    if add_thres == 0:
+        return [lost(feats, dims, scales, init_image_size, k_patches, dynamic_thres, dbscan, add_thres)]
+
+    fake_thres = [ft for ft in range(0, add_thres + 1, int(add_thres / 8))]
+    thres_tmp = [lost(feats, dims, scales, init_image_size, k_patches, dynamic_thres, dbscan, thres) for thres in
+                 fake_thres]
+    return thres_tmp
+
+
+def dbscan_filter(patches_xy):
+    if len(patches_xy) == 0:
+        return []
+    clustering = DBSCAN(eps=1, min_samples=5).fit(patches_xy)
+    labels = clustering.labels_
+    from collections import Counter
+    element_counts = Counter(labels)
+    most_common_element, count = element_counts.most_common(1)[0]
+    if most_common_element == -1:
+        try:
+            most_common_element, _ = element_counts.most_common(2)[1]
+        except:
+            return []
+    return [patches_xy[i] for i in range(len(labels)) if labels[i] == most_common_element]
+
+
+def patch_scoring(M, dynamic_threshold, k_patches, add_thres=0):
+    """
+    Patch scoring based on the inverse degree.
+        dynamic_threshold: set to True will override the threshold value by mean of the matrix
+    """
+    # add_thres = -150
+    threshold = torch.mean(M) + add_thres if dynamic_threshold else 0.0
+
+    A = M.clone()
+    A.fill_diagonal_(0)
+    A[A < threshold] = 0
+    cent = -torch.sum(A > threshold, dim=1).float()
+    sel = torch.argsort(cent, descending=True)
+    cent = cent[sel]
+
+    # Dynamic Patch Selection (if `k_patches == -1`)
+    if k_patches == -1:
+        jumps = [abs(int(float((cent[i] - cent[i - 1]).cpu().numpy()))) for i in range(1, len(cent))]
+
+        # replace first 10% of the jumps with 0
+        num_10_percent = int(len(jumps) * 0.025)
+        jumps[:num_10_percent] = [0] * num_10_percent
+
+        # replace last 10% of the jumps with 0
+        jumps[-num_10_percent:] = [0] * num_10_percent
+
+        k_jump = get_last_argmax(jumps)
+
+        #plot cent
+        plt.plot(cent.cpu().numpy())
+
+        return sel[:k_jump], cent, jumps
+
+    return sel, cent, None
+
+
+def get_last_argmax(lst):
+    '''
+        [1, 4, 4, 4, 3, 4] --> return 5
+        default argmax would return 1
+    '''
+    lst2 = lst[::-1]
+    return len(lst) - lst2.index(max(lst)) - 1
+
+
+def detect_box(dims, object_patches, initial_im_size=None, scales=None):
+    """
+    Extract a box corresponding to the seed patch. Among connected components extract from the affinity matrix, select the one corresponding to the seed patch.
+    """
+    if len(object_patches) == 0:
+        return [0, 0, 0, 0], [0, 0, 0, 0]
+    object_patches_unravel = [np.unravel_index(p, dims) for p in object_patches]
+    mask = np.zeros(dims)
+    for patch_id in object_patches_unravel:
+        mask[patch_id] = 1
+
+    mask = np.where(mask == 1)
+    ymin, ymax = min(mask[0]), max(mask[0]) + 1
+    xmin, xmax = min(mask[1]), max(mask[1]) + 1
+
+    r_xmin, r_xmax = scales[1] * xmin, scales[1] * xmax
+    r_ymin, r_ymax = scales[0] * ymin, scales[0] * ymax
+    pred = [r_xmin, r_ymin, r_xmax, r_ymax]
+
+    if initial_im_size:
+        pred[2] = min(pred[2], initial_im_size[1])
+        pred[3] = min(pred[3], initial_im_size[0])
+
+    return pred, [ymin, xmin, ymax, xmax]
+
+
+def dino_seg(attn, dims, patch_size, head=0):
+    """
+    Extraction of boxes based on the DINO segmentation method proposed in https://github.com/facebookresearch/dino.
+    Modified from https://github.com/facebookresearch/dino/blob/main/visualize_attention.py
+    """
+    w_featmap, h_featmap = dims
+    nh = attn.shape[1]
+    official_th = 0.6
+
+    attentions = attn[0, :, 0, 1:].reshape(nh, -1)
+    val, idx = torch.sort(attentions)
+    val /= torch.sum(val, dim=1, keepdim=True)
+    cumval = torch.cumsum(val, dim=1)
+    th_attn = cumval > (1 - official_th)
+    idx2 = torch.argsort(idx)
+    for h in range(nh):
+        th_attn[h] = th_attn[h][idx2[h]]
+    th_attn = th_attn.reshape(nh, w_featmap, h_featmap).float()
+
+    labeled_array, num_features = scipy.ndimage.label(th_attn[head].cpu().numpy())
+    size_components = [np.sum(labeled_array == c) for c in range(np.max(labeled_array))]
+
+    biggest_component = np.argmax(size_components[1:]) + 1 if len(size_components) > 1 else 0
+    mask = np.where(labeled_array == biggest_component)
+
+    ymin, ymax = min(mask[0]), max(mask[0]) + 1
+    xmin, xmax = min(mask[1]), max(mask[1]) + 1
+
+    r_xmin, r_xmax = xmin * patch_size, xmax * patch_size
+    r_ymin, r_ymax = ymin * patch_size, ymax * patch_size
+    return [r_xmin, r_ymin, r_xmax, r_ymax]
---
+++ LOST/visualizations.py
@@ -0,0 +1,191 @@
+# Copyright 2021 - Valeo Comfort a nd Driving Assistance - Oriane Siméoni @ valeo.ai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cv2
+import torch
+import skimage.io
+import numpy as np
+import torch.nn as nn
+from PIL import Image
+import os
+import matplotlib.pyplot as plt
+
+def visualize_predictions(image, pred, scales, dims, vis_folder, im_name, plot_score=False, potentials=None, jumps=None, add_thres=0,scores=None,k=0):
+    """
+    Visualization of the predicted box and the corresponding seed patch.
+    """
+    image_copy = image.copy()
+
+    w_featmap, h_featmap = dims
+    # Plot the box
+    cv2.rectangle(
+        image_copy,
+        (int(pred[0]), int(pred[1])),
+        (int(pred[2]), int(pred[3])),
+        (255, 0, 0), 3,
+    )
+    im_name = im_name.split("\\")[-1]
+    # pltname = f"{vis_folder}/LOST_{im_name}.png"
+    # Image.fromarray(image).save(pltname)
+    # print(f"Predictions saved at {pltname}.")
+
+
+    if potentials is not None:
+        # plot half of potentials
+        for seed in potentials[:len(potentials)//2]:
+            if type(seed) == torch.Tensor:
+                s = np.unravel_index(seed.cpu().numpy(), (w_featmap, h_featmap))
+            else:
+                s = np.unravel_index(seed, (w_featmap, h_featmap))
+            size = np.asarray(scales) / 2
+            cv2.rectangle(
+                image_copy,
+                (int(s[1] * scales[1] - (size[1] / 2)), int(s[0] * scales[0] - (size[0] / 2)),
+                ),
+                (int(s[1] * scales[1] + (size[1] / 2)), int(s[0] * scales[0] + (size[0] / 2)),
+                ),
+                (128, 0, 128), 1, # Purple
+            )
+        # plot the other half of potentials
+        for seed in potentials[len(potentials)//2:]:
+            if type(seed) == torch.Tensor:
+                s = np.unravel_index(seed.cpu().numpy(), (w_featmap, h_featmap))
+            else:
+                s = np.unravel_index(seed, (w_featmap, h_featmap))
+            size = np.asarray(scales) / 2
+            cv2.rectangle(
+                image_copy,
+                (int(s[1] * scales[1] - (size[1] / 2)), int(s[0] * scales[0] - (size[0] / 2)),
+                ),
+                (int(s[1] * scales[1] + (size[1] / 2)), int(s[0] * scales[0] + (size[0] / 2)),
+                ),
+                (0, 128, 128), 1, # Teal
+            )
+
+        # visualize paths at id= k_jump
+        patch_k_jump = potentials[-1] if len(potentials) > 0 else 0
+        s = np.unravel_index(patch_k_jump, (w_featmap, h_featmap))
+        size = np.asarray(scales) / 2
+        cv2.rectangle(
+            image_copy,
+            (int(s[1] * scales[1] - (size[1] / 2)), int(s[0] * scales[0] - (size[0] / 2)),
+            ),
+            (int(s[1] * scales[1] + (size[1] / 2)), int(s[0] * scales[0] + (size[0] / 2)),
+            ),
+            (0, 0, 255), -1, # Blue
+        )
+        os.makedirs(f"{vis_folder}/{im_name}", exist_ok=True)
+        pltname = f"{vis_folder}/{im_name}/add_{add_thres}_potentials.png"
+        Image.fromarray(image_copy).save(pltname)
+        print(f"Predictions saved at {pltname}.")
+        # save jumps plot
+        # plt.figure(figsize=(10, 6))
+        # plt.plot(jumps, marker='o', linestyle='-', color='b')
+        # plt.title('Plot of Jumps')
+        # plt.xlabel('Index')
+        # plt.ylabel('Jump Value')
+        # plt.grid(True)
+        os.makedirs(f"{vis_folder}/{im_name}/scores", exist_ok=True)
+        scores = scores.cpu().numpy()
+        plt.plot(scores)
+        plt.title('Plot of Scores')
+        plt.xlabel('Index')
+        plt.ylabel('Score Value')
+        plt.plot(k, scores[k], 'ro')
+        if plot_score:
+            plt.savefig(f"{vis_folder}/{im_name}/scores/jumps_add_{add_thres}.png")
+
+
+def visualize_fms(A, seed, scores, dims, scales, output_folder, im_name):
+    """
+    Visualization of the maps presented in Figure 2 of the paper. 
+    """
+    w_featmap, h_featmap = dims
+
+    # Binarized similarity
+    binA = A.copy()
+    binA[binA < 0] = 0
+    binA[binA > 0] = 1
+
+    # Get binarized correlation for this pixel and make it appear in gray
+    im_corr = np.zeros((3, len(scores)))
+    where = binA[seed, :] > 0
+    im_corr[:, where] = np.array([128 / 255, 133 / 255, 133 / 255]).reshape((3, 1))
+    # Show selected pixel in green
+    im_corr[:, seed] = [204 / 255, 37 / 255, 41 / 255]
+    # Reshape and rescale
+    im_corr = im_corr.reshape((3, w_featmap, h_featmap))
+    im_corr = (
+        nn.functional.interpolate(
+            torch.from_numpy(im_corr).unsqueeze(0),
+            scale_factor=scales,
+            mode="nearest",
+        )[0].cpu().numpy()
+    )
+
+    # Save correlations
+    skimage.io.imsave(
+        fname=f"{output_folder}/corr_{im_name}.png",
+        arr=im_corr.transpose((1, 2, 0)),
+    )
+    print(f"Image saved at {output_folder}/corr_{im_name}.png .")
+
+    # Save inverse degree
+    im_deg = (
+        nn.functional.interpolate(
+            torch.from_numpy(1 / binA.sum(-1)).reshape(1, 1, w_featmap, h_featmap),
+            scale_factor=scales,
+            mode="nearest",
+        )[0][0].cpu().numpy()
+    )
+    plt.imsave(fname=f"{output_folder}/deg_{im_name}.png", arr=im_deg)
+    print(f"Image saved at {output_folder}/deg_{im_name}.png .")
+
+def visualize_seed_expansion(image, pred, seed, pred_seed, scales, dims, vis_folder, im_name):
+    """
+    Visualization of the seed expansion presented in Figure 3 of the paper. 
+    """
+    w_featmap, h_featmap = dims
+
+    # Before expansion
+    cv2.rectangle(
+        image,
+        (int(pred_seed[0]), int(pred_seed[1])),
+        (int(pred_seed[2]), int(pred_seed[3])),
+        (204, 204, 0),  # Yellow
+        3,
+    )
+
+    # After expansion
+    cv2.rectangle(
+        image,
+        (int(pred[0]), int(pred[1])),
+        (int(pred[2]), int(pred[3])),
+        (204, 0, 204),  # Magenta
+        3,
+    )
+
+    # Position of the seed
+    center = np.unravel_index(seed.cpu().numpy(), (w_featmap, h_featmap))
+    start_1 = center[0] * scales[0]
+    end_1 = center[0] * scales[0] + scales[0]
+    start_2 = center[1] * scales[1]
+    end_2 = center[1] * scales[1] + scales[1]
+    image[start_1:end_1, start_2:end_2, 0] = 204
+    image[start_1:end_1, start_2:end_2, 1] = 37
+    image[start_1:end_1, start_2:end_2, 2] = 41
+
+    pltname = f"{vis_folder}/LOST_seed_expansion_{im_name}.png"
+    Image.fromarray(image).save(pltname)
+    print(f"Image saved at {pltname}.")
